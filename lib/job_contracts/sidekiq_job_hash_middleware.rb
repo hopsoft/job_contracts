d3f932fcfc11175508ae8f6732113320275c5c94
Hopsoft <natehop@gmail.com>
2022-05-04 11:58:11 -0600

    Make code review happy

--- lib/job_contracts/sidekiq_job_hash_middleware.rb
+++ lib/job_contracts/sidekiq_job_hash_middleware.rb
@@ -2,7 +2,7 @@
 
 module JobContracts
   class SidekiqJobHashMiddleware
-    def call(worker, job, queue)
+    def call(worker, job, _queue)
       worker.instance_variable_set :@sidekiq_job_hash, job
       yield
     end
